9d3518346f57d2e529adaa7c591e6a640c08d443
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-11 13:43:18 -0600

    Componete de Email

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,6 @@
+<div class="container mx-auto px-5 bg-cyan-50	">
+    <h1 class="text-3xl">Información de: {{$title}}</h1>
+    <div class="grid grid-cols-2 gap-2">
+        {{ $slot }}
+    </div>
+</div>
