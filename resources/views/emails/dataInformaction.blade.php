a62041b0f1fcff5f2ac341a71d49c431d36f2671
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-11 13:30:21 -0600

    Visual del correo generada #2

--- resources/views/emails/dataInformaction.blade.php
+++ resources/views/emails/dataInformaction.blade.php
@@ -1,29 +1,44 @@
 <html>
-    <div class="container mx-auto px-4">
-        <h1 class="text-3xl">Información: </h1>
-        <span class="font-semibold">Nombre: </span>
-        <p class=""> {{$name}}</p>
-        <br>
-        <span class="font-semibold">Calle: </span>
-        <p class=""> {{$street}}</p>
-        <br>
-        <span class="font-semibold">Código postal: </span>
-        <p class=""> {{$code}}</p>
-        <br>
-        <span class="font-semibold">Número exterior: </span>
-        <p class=""> {{$numext}}</p>
-        <br>
-        <span class="font-semibold">Número interior: </span>
-        <p class=""> {{$numint}}</p>
-        <br>
-        <span class="font-semibold">País: </span>
-        <p class=""> {{$country}}</p>
-        <br>
-        <span class="font-semibold">Descripción: </span>
-        <p class=""> {{$description}}</p>
-        <br>
-        @foreach ($images as $item)
-            <img src="{{asset('storage/imgs/' . $item)}}" alt="..." width="100" height="100">
-        @endforeach
+    <div class="container mx-auto px-5 bg-cyan-50	">
+      <h1 class="text-3xl">Información: </h1>
+      <div class="grid grid-cols-2 gap-2">
+        <div class="m-8">
+          <span class="font-semibold">Nombre: </span>
+          <p class=""> {{$name}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">Calle: </span>
+          <p class=""> {{$street}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">Código postal: </span>
+          <p class=""> {{$code}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">Número exterior: </span>
+          <p class=""> {{$numext}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">Número interior: </span>
+          <p class=""> {{$numint}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">País: </span>
+          <p class=""> {{$country}}</p>
+        </div>
+        <div class="m-8">
+          <span class="font-semibold">Descripción: </span>
+          <p class=""> {{$description}}</p>
+        </div>
+      </div>
+    <div class="grid grid-cols-6 gap-6 mx-3">
+      <div>
+        <div class="grid justify-items-center">
+            @foreach ($images as $item)
+                <img src="{{asset('storage/imgs/' . $item)}}" alt="..." width="100" height="100">
+            @endforeach
+        </div>
+      </div>
+    </div>
     </div>
 </html>
